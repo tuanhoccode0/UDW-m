495b97a595a10324fd1acf9a80b1313a7b94ddf1
tuanhoccode0 <nguyenanhtuan03062007@gmail.com>
2026-08-23 09:38:57 +0000

    aaa

---
+++ JS/js08.js
@@ -0,0 +1,10 @@
+document.getElementById("btnThucHien").onclick = function () {
+    const bangChu = document.getElementById("bangChu");
+
+    // Đổi nội dung
+    bangChu.textContent = document.getElementById("noiDung").value;
+
+    // Đổi màu nền và màu chữ (dùng thuộc tính style của phần tử)
+    bangChu.style.backgroundColor = document.getElementById("mauNen").value;
+    bangChu.style.color = document.getElementById("mauChu").value;
+};
